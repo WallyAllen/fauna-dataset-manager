ee91fd1e9b99a4fd571cff56fb34336739e81aed
MezaJoaq <mezajoaquin815@gmail.com>
2026-04-21 10:50:59 +0000

    [3.A] Verificacion de parametros y diferenciacion de coordenadas (sin terminar)

--- src/validaciones.py
+++ src/validaciones.py
@@ -1,25 +1,26 @@
 from pathlib import Path
 import csv
 # EJERCICIO 3.A
-def validar_coordenadas(csv):
+def validar_coordenadas(csv,nombreColumna):
     cant_inv = 0
     list_inv = []
     colum_vacia = True
     # Compruebo que el tipo de dato es un iterador
     if hasattr(csv, "__iter__"):
         print("Buscando errores en los datos de latitud...")
-        # Recorre la columna decimalLatitude para buscar errores
+        # Recorre la columna enviada para buscar errores
         for fila in csv:
             # Evaluo si existe un valor en ese registro
-            valor = fila["decimalLatitude"]
-            if valor != '':
-                colum_vacio = False
+            valor = fila[nombreColumna]
+            if valor != '' and 'latitude' in nombreColumna:
+                colum_vacia = False
                 latitud = float(valor)
                 if latitud < -90 or latitud > 90:
                     cant_inv += 1
                     list_inv.append(latitud)
+            else: print('El nombre de la columna es incorrecto o no existe en el dataset')
         
-        if colum_vacia == True:
+        if colum_vacia:
             print("No existen valores en la columna 'decimalLatitude'")
     else:
         print("El tipo de dato no es un iterador")
@@ -30,11 +31,13 @@ if __name__ == "__main__":
     #Creo una variable con la ruta de archivo dinamica
     DIC_BASE = Path(__file__).resolve().parent.parent
 
-    file_route = DIC_BASE / 'raw_datasets' / 'coleccion-ornitologica' / 'occurrence.txt'
+    file_route = DIC_BASE / 'raw_datasets' / 'bird-sounds' / 'Occurrence.txt'
 
     with open(file_route, "r") as file:
-        csv_file = csv.DictReader(file,delimiter='\t')
-        validar_coordenadas(csv_file)
+        csv_file = csv.DictReader(file,delimiter=',')
+        print(csv_file.fieldnames)
+        dato = input('Ingrese que columna quiere verificar:')
+        validar_coordenadas(csv_file,dato)
 
 
         # Comprobacion para saber si se cargo bien el archivo
