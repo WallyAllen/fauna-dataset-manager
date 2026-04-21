cbaf4ce09eb458ba20a1003d166060b4adc5753d
MezaJoaq <mezajoaquin815@gmail.com>
2026-04-21 12:11:35 +0000

    Ejercicio 3.A terminado - Creo funcion para validar parametros
    Las funciones tienen que reciber como parametro un iterador y el nombre
    de la columna, verifico ambos datos
    
    El ejercicio 3.A tal vez se me olvido algun dato que pueda llegar a dar
    error. Cualquier sugerencia viene bien :)

--- src/validaciones.py
+++ src/validaciones.py
@@ -1,33 +1,50 @@
 from pathlib import Path
 import csv
-# EJERCICIO 3.A
+# Funcion para verificar que el iterador enviado es correcto
+def validar_archivo(csv,nombreColumna):
+    # Compruebo que el tipo de dato es un iterador
+    if not hasattr(csv, "__iter__"):
+        print("El tipo de dato no es un iterador")
+        return True
+    # Compruebo que exista la columna enviada
+    if nombreColumna not in csv.fieldnames:
+        print("La columna es incorrecta o no existe en el dataset")
+        return True
+return False
+# 3.A
 def validar_coordenadas(csv,nombreColumna):
     cant_inv = 0
     list_inv = []
-    colum_vacia = True
-    # Compruebo que el tipo de dato es un iterador
-    if hasattr(csv, "__iter__"):
-        print("Buscando errores en los datos de latitud...")
-        # Recorre la columna enviada para buscar errores
-        for fila in csv:
-            # Evaluo si existe un valor en ese registro
-            valor = fila[nombreColumna]
-            if valor != '' and 'latitude' in nombreColumna:
-                colum_vacia = False
-                latitud = float(valor)
-                if latitud < -90 or latitud > 90:
+    colum_vacio = True
+    print("Buscando errores en los datos de latitud...")
+    if validar_archivo(csv,nombreColumna): return cant_inv, list_inv
+    # Recorre la columna enviada para buscar errores
+    for fila in csv:
+    # Evaluo si existe un valor en ese registro
+        valor = fila[nombreColumna]
+        if valor != '':
+            colum_vacia = False
+            coord = float(valor)
+            if 'latitude' in nombreColumna:
+                if coord < -90 or coord > 90:
                     cant_inv += 1
-                    list_inv.append(latitud)
-            else: print('El nombre de la columna es incorrecto o no existe en el dataset')
-        
-        if colum_vacia:
-            print("No existen valores en la columna 'decimalLatitude'")
-    else:
-        print("El tipo de dato no es un iterador")
-        
+                    list_inv.append(coord)
+            if 'longitude' in nombreColumna:
+                if coord < -180 or coord > 180:
+                    cant_inv += 1
+                    list_inv.append(coord)
+
+    if colum_vacia:
+        print("No existen valores en la columna o la columna enviada en invalida")
+    return cant_inv, list_inv
+
+#3.B
+def constatar_coordenadas()
 
 #Bloque para probar las funciones de validacion
 if __name__ == "__main__":
+    list = []
+    cant = 0
     #Creo una variable con la ruta de archivo dinamica
     DIC_BASE = Path(__file__).resolve().parent.parent
 
@@ -37,19 +54,9 @@ if __name__ == "__main__":
         csv_file = csv.DictReader(file,delimiter=',')
         print(csv_file.fieldnames)
         dato = input('Ingrese que columna quiere verificar:')
-        validar_coordenadas(csv_file,dato)
-
-
-        # Comprobacion para saber si se cargo bien el archivo
-        """
-        i = 0
-        for fila in csv_file:
-            i += 1
-            if i < 2:
-                print(f"La localidad es {fila['locality']}")
-                print(f"La latitud es {fila['decimalLatitude']}")
-            else:
-                break
-        """
+        cant, lista = validar_coordenadas(csv_file,dato)
+        print(f"La cantidad de registro invalidos son {cant}")
+        for i in range(len(lista)):
+            print(f"Los registros invalidos son {lista[i]})
 
 
