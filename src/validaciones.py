d538d2745da7e9e8c3099fd59f4def99aa7e3d4e
MezaJoaq <mezajoaquin815@gmail.com>
2026-04-21 00:29:13 +0000

    [3.A]Logica de funcion para buscar datos invalidos de coordenadas (Falta terminar)

--- src/validaciones.py
+++ src/validaciones.py
@@ -1,7 +1,29 @@
-#EJERCICIO 3.A
 from pathlib import Path
 import csv
-
+# EJERCICIO 3.A
+def validar_coordenadas(csv):
+    cant_inv = 0
+    list_inv = []
+    colum_vacia = True
+    # Compruebo que el tipo de dato es un iterador
+    if hasattr(csv, "__iter__"):
+        print("Buscando errores en los datos de latitud...")
+        # Recorre la columna decimalLatitude para buscar errores
+        for fila in csv:
+            # Evaluo si existe un valor en ese registro
+            valor = fila["decimalLatitude"]
+            if valor != '':
+                colum_vacio = False
+                latitud = float(valor)
+                if latitud < -90 or latitud > 90:
+                    cant_inv += 1
+                    list_inv.append(latitud)
+        
+        if colum_vacia == True:
+            print("No existen valores en la columna 'decimalLatitude'")
+    else:
+        print("El tipo de dato no es un iterador")
+        
 
 #Bloque para probar las funciones de validacion
 if __name__ == "__main__":
@@ -9,10 +31,14 @@ if __name__ == "__main__":
     DIC_BASE = Path(__file__).resolve().parent.parent
 
     file_route = DIC_BASE / 'raw_datasets' / 'coleccion-ornitologica' / 'occurrence.txt'
-    print(f"Buscando en {file_route}")
+
     with open(file_route, "r") as file:
         csv_file = csv.DictReader(file,delimiter='\t')
-        type(csv_file)
+        validar_coordenadas(csv_file)
+
+
+        # Comprobacion para saber si se cargo bien el archivo
+        """
         i = 0
         for fila in csv_file:
             i += 1
@@ -21,4 +47,6 @@ if __name__ == "__main__":
                 print(f"La latitud es {fila['decimalLatitude']}")
             else:
                 break
+        """
+
 
