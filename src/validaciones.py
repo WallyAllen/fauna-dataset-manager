c7f5ef244006f0d8fd83cd4dfe1e9916ca1b0a3b
MezaJoaq <mezajoaquin815@gmail.com>
2026-04-22 00:15:28 +0000

    Ejercicio 3.A corregido - cambio en parametros de la funcion

--- src/validaciones.py
+++ src/validaciones.py
@@ -1,45 +1,53 @@
 from pathlib import Path
 import csv
-# Funcion para verificar que el iterador enviado es correcto
-def validar_archivo(csv,nombreColumna):
-    # Compruebo que el tipo de dato es un iterador
-    if not hasattr(csv, "__iter__"):
-        print("El tipo de dato no es un iterador")
-        return True
-    # Compruebo que exista la columna enviada
-    if nombreColumna not in csv.fieldnames:
-        print("La columna es incorrecta o no existe en el dataset")
-        return True
-return False
+# Funcion para abrir el archivo con el path enviado
+def evaluar_error(valor,nombreColumna):
+    error = False
+
+    if 'latitude' in nombreColumna:
+        if valor < -90 or valor > 90:
+            error = True
+    if 'longitude' in nombreColumna:
+        if valor < -180 or valor > 180:
+            error = True
+    return error
 # 3.A
-def validar_coordenadas(csv,nombreColumna):
+def validar_coordenadas(path,nombreColumna,delimitador):
     cant_inv = 0
     list_inv = []
     colum_vacio = True
     print("Buscando errores en los datos de latitud...")
-    if validar_archivo(csv,nombreColumna): return cant_inv, list_inv
-    # Recorre la columna enviada para buscar errores
-    for fila in csv:
-    # Evaluo si existe un valor en ese registro
-        valor = fila[nombreColumna]
-        if valor != '':
-            colum_vacia = False
-            coord = float(valor)
-            if 'latitude' in nombreColumna:
-                if coord < -90 or coord > 90:
+    if delimitador == "\\t" or delimitador == "/t" : delimitador = "\t"
+
+    with open(path, "r") as file:
+        csv_reader = csv.DictReader(file,delimiter = delimitador)
+
+        if nombreColumna not in csv_reader.fieldnames:
+            print(f"La columna {nombreColumna} no existe en el dataset")
+            return cant_inv, list_inv
+
+        # Recorre la columna enviada para buscar errores
+        for fila in csv_reader:
+        # Evaluo si existe un valor en ese registro
+            valor = fila[nombreColumna]
+            if valor != '':
+                colum_vacia = False
+                # Utilizo el try/except para valores que no son numeros
+                try:
+                    coord = float(valor)
+                    if evaluar_error(coord,nombreColumna):
+                        cant_inv += 1
+                        list_inv.append(coord)
+                except (ValueError, TypeError):
                     cant_inv += 1
-                    list_inv.append(coord)
-            if 'longitude' in nombreColumna:
-                if coord < -180 or coord > 180:
-                    cant_inv += 1
-                    list_inv.append(coord)
+                    list_inv.append(valor)
 
     if colum_vacia:
         print("No existen valores en la columna o la columna enviada en invalida")
     return cant_inv, list_inv
 
 #3.B
-def constatar_coordenadas()
+#def constatar_coordenadas()
 
 #Bloque para probar las funciones de validacion
 if __name__ == "__main__":
@@ -50,13 +58,11 @@ if __name__ == "__main__":
 
     file_route = DIC_BASE / 'raw_datasets' / 'bird-sounds' / 'Occurrence.txt'
 
-    with open(file_route, "r") as file:
-        csv_file = csv.DictReader(file,delimiter=',')
-        print(csv_file.fieldnames)
-        dato = input('Ingrese que columna quiere verificar:')
-        cant, lista = validar_coordenadas(csv_file,dato)
-        print(f"La cantidad de registro invalidos son {cant}")
-        for i in range(len(lista)):
-            print(f"Los registros invalidos son {lista[i]})
+    dato = input('Ingrese que columna quiere verificar:')
+    delimitador = input('Ingrese el delimitador del dataset:')
+    cant, lista = validar_coordenadas(file_route,dato,delimitador)
+    print(f"La cantidad de registro invalidos son {cant}")
+    for i in range(len(lista)):
+        print(f"Los registros invalidos son {lista[i]}")
 
 
