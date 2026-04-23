fd07b8b40d53fc0675d2db2cb7122735a565407e
Rocco <roccomili.rm@gmail.com>
2026-04-23 14:11:38 -0300

    feat: modificada la landing, añadido un texto base

---
+++ 01_Inicio.py
@@ -0,0 +1,45 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = "PLACEHOLDER",
+    page_icon = ":herb:",
+    layout = "wide"
+)
+
+st.title("🌿 PLACEHOLDER", text_alignment="center")
+
+st.header("¿Qué hace PLACEHOLDER?", divider=True)
+st.write(""" 
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
+    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+    culpa qui officia deserunt mollit anim id est laborum.
+
+    Nullam tortor enim, accumsan non urna eu, hendrerit egestas purus. Maecenas viverra est et aliquam porttitor.
+    Duis neque augue, condimentum in nibh a, laoreet faucibus ligula. Cras congue tristique arcu vitae interdum.
+    Sed at massa tempus, sollicitudin ex vel, bibendum ante. Vestibulum iaculis consequat neque, nec vulputate 
+    libero porta ac. Nulla eleifend odio nec elit faucibus, nec facilisis lacus rutrum. Fusce nec mauris eget 
+    dolor laoreet mollis vel vestibulum ligula. Sed sapien arcu, ornare id finibus ut, porttitor eu ante. 
+    Vestibulum leo libero, mollis accumsan fermentum ac, suscipit a ligula. Orci varius natoque penatibus et 
+    magnis dis parturient montes, nascetur ridiculus mus. Proin quis tincidunt sapien.
+    """)
+st.divider()    
+
+st.header("Acerca de Darwin Core", divider=True)
+st.write(""" 
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
+    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+    culpa qui officia deserunt mollit anim id est laborum.
+
+    Nullam tortor enim, accumsan non urna eu, hendrerit egestas purus. Maecenas viverra est et aliquam porttitor.
+    Duis neque augue, condimentum in nibh a, laoreet faucibus ligula. Cras congue tristique arcu vitae interdum.
+    Sed at massa tempus, sollicitudin ex vel, bibendum ante. Vestibulum iaculis consequat neque, nec vulputate 
+    libero porta ac. Nulla eleifend odio nec elit faucibus, nec facilisis lacus rutrum. Fusce nec mauris eget 
+    dolor laoreet mollis vel vestibulum ligula. Sed sapien arcu, ornare id finibus ut, porttitor eu ante. 
+    Vestibulum leo libero, mollis accumsan fermentum ac, suscipit a ligula. Orci varius natoque penatibus et 
+    magnis dis parturient montes, nascetur ridiculus mus. Proin quis tincidunt sapien.
+    """)
+st.divider()
