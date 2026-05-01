8b8df8ae568ece6288e645f8efc6888857665914
ValenPerz <valentinoperazzo7@gmail.com>
2026-05-01 17:54:07 -0300

    Ajuste de docstring

--- src/insercion.py
+++ src/insercion.py
@@ -5,10 +5,6 @@ def create_record_structure(filepath, id_column='id', encoding='utf-8', delimite
     Lee el encabezado del dataset y retorna un diccionario con sus columnas 
     (con valores vacíos por defecto) para representar la estructura de un nuevo registro.
     Se excluye la columna de identificación (ID) pasada por parámetro.
-    
-    Esta estructura (diccionario) es la forma más flexible en Python para 
-    representar registros de Darwin Core, ya que las columnas pueden variar 
-    entre cada uno de los datasets.
     """
     with open(filepath, encoding=encoding) as f:
         reader = csv.DictReader(f, delimiter=delimiter)
