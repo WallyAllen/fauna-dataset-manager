5b6917978009559d2a90b965cec1351d479ad7e4
Rocco <roccomili.rm@gmail.com>
2026-05-27 20:19:45 -0300

    feat: 1.C, incisos 1 y 3. known bug: NO tenemos archivos en processed_datasets/. penddiente de editar

--- src/ui_state.py
+++ src/ui_state.py
@@ -1,8 +1,16 @@
 import streamlit as st
 from src.dataset_config import TRADUCTOR_DATASETS
+from src.log_operaciones import current_date
+from src.lectura import load_dataframe
 
 ACTIVE_DATASET_SESSION_KEY = "dataset_activo"
+SELECTION_TIME_KEY = "fecha_seleccion"
 
+def handle_selection_change():
+    """
+    Función callback para ejecutar al cambiar el selectbox
+    """
+    st.session_state[SELECTION_TIME_KEY] = current_date()
 
 def get_current_dataset():
     """
@@ -15,18 +23,31 @@ def get_current_dataset():
 
     if ACTIVE_DATASET_SESSION_KEY not in st.session_state:
         st.session_state[ACTIVE_DATASET_SESSION_KEY] = datasets[0]
+        st.session_state[SELECTION_TIME_KEY] = current_date()
 
     current_dataset = st.session_state[ACTIVE_DATASET_SESSION_KEY]
     if current_dataset not in datasets:
         current_dataset = datasets[0]
         st.session_state[ACTIVE_DATASET_SESSION_KEY] = current_dataset
 
-    selected_dataset = st.sidebar.selectbox(
-        "Dataset activo",
+    with st.sidebar:
+        selected_dataset = st.selectbox(
+        "Seleccioná un dataset",
         options=datasets,
         index=datasets.index(current_dataset),
         key="sidebar_dataset_selector",
+        on_change=handle_selection_change
     )
-    st.session_state[ACTIVE_DATASET_SESSION_KEY] = selected_dataset
+
+        st.session_state[ACTIVE_DATASET_SESSION_KEY] = selected_dataset
+
+        # NOTA: PReguntar si es necesario hacer display, porque ya está en el selectbox
+
+        st.info(f'**DATASET ACTUAL:** {selected_dataset}')
+
+        # PENDIENTE: Ejercicio 1.C, parte 2. Cantidad de registros del dataset.
+        # NO tenemos archivos en processed_datasets/ como para llegar a esto. 
+
+        st.caption(f'Seleccionado el {st.session_state[SELECTION_TIME_KEY]}')
 
     return selected_dataset
